817837a3f063364b42dcfe2c8d00fcececcf1003
Sheena Takkenberg <sheena.takkenberg@gmail.com>
2016-06-20 15:27:56 +1000

    sort of working

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
-function sayHello(){
-  alert("Hello World");
+var num = 0;
+
+function countUp(){
+    num++;
+    var p = document.getElementbyId('countUp');
+    p.innerHTML = '<img src="https://i.ytimg.com/vi/tnt0CGkgt98/maxresdefault.jpg">';
 }
+
+var person = {
+  name: "Sheena",
+  age: 33,
+  runAlert: function(){
+    alert("Hi my name is Sheena");
+  },
+  car: {
+    model: "don't have one",
+    color: "none",
+    engine: {
+      model: "don't have one",
+      weight: 0,
+      weightType: 'metric'
+    }
+  }
+}
+
+alert(person["car"]["engine"]["weight"])
+alert(person.car.engine.weight)
